3b5288b2286345133b4574e3252ade85c52a771c
DavidSanwald <david.sanwald@gmail.com>
2020-07-01 12:18:16 +0200

    add valid social data to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,11 +34,11 @@ module.exports = {
     social: [
       {
         name: `twitter`,
-        url: `https://twitter.com/gatsbyjs`,
+        url: `https://twitter.com/davidsanwald`,
       },
       {
         name: `github`,
-        url: `https://github.com/gatsbyjs`,
+        url: `https://github.com/davidsanwald`,
       },
     ],
   },
